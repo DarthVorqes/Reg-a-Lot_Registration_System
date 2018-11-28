a5d3296c4a0c1af187273be1cec7809a9b1f6d7b
AndrewH <andrewrhands@gmail.com>
2018-11-28 15:53:19 -0600

    Added various classes, properties, empty methods, and created a Windows Forms Project.

---
+++ RegistrationSystem/RegistrationSystem/DatabaseConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationSystem
+{
+    class DatabaseConnection
+    {
+        public DatabaseConnection(int userID, string password)
+        {
+            UserID = userID;
+            HashedPassword = new byte[0];//placeholder
+        }
+        /// <summary>
+        /// this will generally be the student's ID
+        /// </summary>
+        public int UserID { get; set; }
+        byte[] HashedPassword { get; set; }
+    }
+}
---
+++ RegistrationSystem/RegistrationSystem/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationSystem
+{
+    class User
+    {
+        public bool HasPaid { get; private set; }
+        public bool IsProffessor { get; private set; }
+        public bool IsStudent { get; private set; }
+        public bool IsRegistrar { get; private set; }
+        public string Name { get; set; }
+        public int StudentID { get; private set; }
+        public DatabaseConnection Connection { get; set; }
+        Schedule Calender { get; set; }
+        public void ApplyChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
