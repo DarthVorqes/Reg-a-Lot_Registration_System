a287aa0e47c4094440f9e759b746dde528b0200f
Andrew H <andrewrhands@gmail.com>
2018-12-04 13:58:37 -0600

    added custom getters and setters in 'user'

--- RegistrationSystem/RegistrationSystem/User.cs
+++ RegistrationSystem/RegistrationSystem/User.cs
@@ -8,17 +8,119 @@ namespace RegistrationSystem
 {
     class User
     {
-        public virtual int EnterpriseID { get; private set; }
-        public virtual string Name { get; set; }
-        public virtual bool HasPaid { get; private set; }
-        public virtual bool IsProffessor { get; private set; }
-        public virtual bool IsStudent { get; private set; }
-        public virtual bool IsRegistrar { get; private set; }
-        public virtual string Password { get; private set; }
-        public virtual Schedule UserSchedule { get; private set; }
-        public DatabaseConnection Connection { get; private set; }
-        public User Focus{ get; private set; }
-        public void InitializeConnection() =>
-            Connection = new DatabaseConnection(EnterpriseID,Password);
+        //place constructors here
+        //place methods here
+        public void SetFocus(string name)
+        {
+            if (IsRegistrar)
+            {
+                //collect password from DB
+                //initialize Focus
+            }
+        }
+        void EnsurePermissions()
+        {
+            if (checkedPermissions)
+                return;
+            checkedPermissions = true;
+            //collect data from DB
+        }
+        //place properties
+        public int EnterpriseID { get; private set; }
+        public string Name
+        {
+            get
+            {
+                if(name == null)
+                {
+                    //get name from DB
+                }
+                return name;
+            }
+        }
+        public bool HasPaid
+        {
+            get
+            {
+                if (!checkedIfHasPaid)
+                {
+                    //get from DB
+                }
+                return hasPaid;
+            }
+        }
+        public bool IsProfessor
+        {
+            get
+            {
+                EnsurePermissions();
+                return isProfessor;
+            }
+        }
+        public bool IsStudent
+        {
+            get
+            {
+                EnsurePermissions();
+                return isStudent;
+            }
+        }
+        public bool IsRegistrar
+        {
+            get
+            {
+                EnsurePermissions();
+                return isRegistrar;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                if(password == null)
+                {
+                    //get from DB
+                }
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
+        }
+        public Schedule UserSchedule
+        {
+            get
+            {
+                if (UserSchedule == null)
+                {
+                    //get from DB
+                }
+                return UserSchedule;
+            }
+        }
+        public DatabaseConnection Connection
+        {
+            get
+            {
+                if (connection == null)
+                {
+                    //get from DB
+                }
+                return connection;
+            }
+        }
+        public User Focus { get; private set; }
+        //do not touch these VVV go through the properties!
+        string name,
+            password;
+        bool hasPaid,
+            checkedIfHasPaid,
+            checkedPermissions,
+            isProfessor,
+            isStudent,
+            isRegistrar;
+        Schedule userSchedule;
+        DatabaseConnection connection;
     }
 }
