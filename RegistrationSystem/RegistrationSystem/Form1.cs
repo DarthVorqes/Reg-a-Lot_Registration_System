1b8583a115b8496b99482faba0f716d9efa08703
trey <ehbartee@amarillocollege.com>
2018-12-04 13:11:55 -0600

    student wireframe

--- RegistrationSystem/RegistrationSystem/Form1.cs
+++ RegistrationSystem/RegistrationSystem/Form1.cs
@@ -16,5 +16,44 @@ namespace RegistrationSystem
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Trey" && radioButton1.Checked)
+            {
+                studentView SView = new studentView();
+                SView.Show();
+                Hide();
+            }
+
+            if (textBox1.Text == "Trey" && radioButton2.Checked)
+            {
+                string message = "You are not a professor.";
+                string caption = "Authentification Error";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+
+                // Displays the MessageBox.
+
+                result = MessageBox.Show(message, caption, buttons);
+            }
+
+            if (textBox1.Text == "Trey" && radioButton3.Checked)
+            {
+                string message = "You are not a registrar.";
+                string caption = "Authentification Error";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+
+                // Displays the MessageBox.
+
+                result = MessageBox.Show(message, caption, buttons);
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
