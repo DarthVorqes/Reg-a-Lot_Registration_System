2f7e3dbd05e60229f9afb0c98d134766d989d19d
Ian Fabela <a0564508@amarillocollege.com>
2018-12-06 14:21:52 -0600

    changed some names to make changes easier and did some barebones work to the ui

---
+++ RegistrationSystem/RegistrationSystem/Registrar.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistrationSystem
+{
+    public partial class Registrar : Form
+    {
+        public char grade { get; private set; }
+
+        private List<string> sectionIndex = new List<string>() { "section1", "section2", "section3" };
+        private List<string> courseIndex = new List<string>() { "course1", "course2", "course3" };
+        private string userID;
+        
+        public Registrar()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            LogIn logout = new LogIn();
+            logout.Show();
+            Close();
+        }
+
+        private void Registrar_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ProfessorView Professor = new ProfessorView(userID);
+            //StudentView Student = new StudentView();
+            //RegistarView Registar = new RegistarView();
+            if ((string)UserViewComboBox.SelectedItem == "Student")
+            {
+                //Student.show();
+                //Close();
+            }
+            if ((string)UserViewComboBox.SelectedItem == "Professor")
+            {
+                Professor.Show();
+                Close();
+            }
+            if ((string)UserViewComboBox.SelectedItem == "Registar")
+            {
+                //Registar.Show();
+                //Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
