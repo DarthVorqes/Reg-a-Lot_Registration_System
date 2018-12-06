01f880ecb821d8304f01ed578d0b0d5e2f566e01
ClemMax <mfclemence24@amarillocollege.com>
2018-12-06 15:21:48 -0600

    Ready for Registar form, and some real data

--- RegistrationSystem/RegistrationSystem/LogIn.cs
+++ RegistrationSystem/RegistrationSystem/LogIn.cs
@@ -21,34 +21,40 @@ namespace RegistrationSystem
 
         private void LogInButton_Click(object sender, EventArgs e)
         {
-         /*   if (check to see if registar)
-            {
-                RegistarView RView = new RegistarView(ID);
-                RView.Show();
-                Hide();
-            }*/
-            //check to see if professor:
-            if (UserNameTextBox.Text == "Max" || UserNameTextBox.Text == "Al")
-            {
-                //if personID as username this will be fine if not then just search data base for the id linked to the username
-                ID = UserNameTextBox.Text; 
-                ProfessorView PView = new ProfessorView(ID);
-                PView.Show();
-                Hide();
-            }
-          if(UserNameTextBox.Text == "Trey")
-            {
-                StudentView SView = new StudentView();
-                SView.Show();
-                Hide();
-            }
-            else
-            {
-                MessageBox.Show("Invaild Credentials!!");
-            }
-
+            bool login = false;  
+                /*   if (check to see if registar)
+                   {
+                       //if personID as username this will be fine if not then just search data base for the id linked to the username
+                       ID = UserNameTextBox.Text;
+                       login = true;
+                       RegistarView RView = new RegistarView(ID);
+                       RView.Show();
+                       Hide();
+                   }*/
+                //check to see if professor:
+                if (UserNameTextBox.Text == "Max")
+                {
+                    //if personID as username this will be fine if not then just search data base for the id linked to the username
+                    ID = UserNameTextBox.Text;
+                    login = true;
+                    ProfessorView PView = new ProfessorView(ID);
+                    PView.Show();
+                    Hide();
+                }
+                if (UserNameTextBox.Text == "Trey")
+                {
+                    //if personID as username this will be fine if not then just search data base for the id linked to the username
+                    ID = UserNameTextBox.Text;
+                    login = true;
+                    studentView SView = new studentView();
+                    SView.Show();
+                    Hide();
+                }
+                if (login == false)
+                {
+                    MessageBox.Show("Invaild Credintials!!");
+                }           
         }
-
         private void ExitButton_Click(object sender, EventArgs e)
         {
             Environment.Exit(1);
